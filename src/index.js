0cd741b8fd4420027f75295c5aecc3f4faa9bb3e
Simerpreet Kaur <simerpreet.kohli25@gmail.com>
2022-09-05 17:16:26 +0530

    context replacement for redux

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import ProductsProvider from "./context/ProductsContext";
+import "./index.css";
+import App from "./App";
+import productReducer from "./store/reducers/products";
+
+// const rootReducer = combineReducers({
+//   shop: productReducer,
+// });
+
+// const store = createStore(rootReducer);
+
+ReactDOM.render(
+  // <Provider store={store}>
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>,
+  // </Provider>
+  document.getElementById("root")
+);
